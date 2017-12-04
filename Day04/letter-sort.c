94e000e8187c0f2044cab2b3c7f8a7ce1e577891
thorstel <thorstil@gmail.com>
2017-12-04 20:06:19 +0100

    (Day 4) A small C program to be used in part 2.
    It will sort the letters of input words and its output can be piped to
    the solution of part 1.
    
    This is pretty much overkill, but now I have a nice little example
    program for how to use the standard qsort function.

---
+++ Day04/letter-sort.c
@@ -0,0 +1,44 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static int cmp_chars(const void* arg1, const void* arg2)
+{
+    const char c1 = *(const char*)arg1;
+    const char c2 = *(const char*)arg2;
+
+    if      (c1 < c2) return -1;
+    else if (c1 > c2) return  1;
+    else              return  0;
+}
+
+int main(void)
+{
+    const size_t buf_size = 64;
+    char         buf[buf_size];
+
+    int    c;
+    size_t i = 0;
+    while (((c = getc(stdin)) != EOF) && (i < buf_size))
+    {
+        if (isalnum(c))
+        {
+            buf[i++] = (char)c;
+        }
+        else if (i > 0)
+        {
+            buf[i] = '\0';
+            qsort(buf, i, sizeof(char), cmp_chars);
+            printf("%s ", buf);
+            i = 0;
+        }
+        else { /* ignore other characters */ }
+
+        if ((char)c == '\n') { putc('\n', stdout); }
+    }
+
+    assert(i < buf_size);
+    return 0;
+}
+
