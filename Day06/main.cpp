f7c3121fb9f29294b0ca41015c5673943ff66520
thorstel <thorstil@gmail.com>
2017-12-06 23:01:27 +0100

    (Day 6) More cleanup.
    There is no reason to always keep track of the current max value. Just
    use the STL function to find the max.
    Use for-loop instead of 'while' and always try to count up.
    Use iterators instead of accessing via index counter.

--- Day06/main.cpp
+++ Day06/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <iterator>
 #include <map>
@@ -6,32 +7,18 @@
 int main(void)
 {
     std::vector<int> banks { std::istream_iterator<int> { std::cin }, {} };
-    const auto bank_size = banks.size();
-
-    auto max_pos = 0u;
-    for (auto i = 0u; i < bank_size; ++i)
-    {
-        if (banks[max_pos] < banks[i]) { max_pos = i; }
-    }
-
     std::map<std::vector<int>, size_t> known;
+
     for (size_t count = 0u; known.emplace(banks, count).second; ++count)
     {
-        auto to_distribute = banks[max_pos];
-        auto curr_pos      = (max_pos + 1) % bank_size;
-        banks[max_pos]     = 0;
+        auto iter = std::max_element(banks.begin(), banks.end());
+        const auto to_distribute = *iter;
+        *iter = 0;
 
-        while (to_distribute > 0)
+        for (auto i = 0; i < to_distribute; ++i)
         {
-            ++banks[curr_pos];
-            if ((banks[curr_pos] > banks[max_pos]) ||
-                ((banks[curr_pos] == banks[max_pos]) && (curr_pos < max_pos)))
-            {
-                max_pos = curr_pos;
-            }
-
-            curr_pos = (curr_pos + 1) % bank_size;
-            --to_distribute;
+            if (++iter == banks.end()) { iter = banks.begin(); }
+            *iter += 1;
         }
     }
 
