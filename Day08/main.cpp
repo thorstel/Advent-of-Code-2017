498528124b998a742a44d7aeb9806075500e32d3
thorstel <thorstil@gmail.com>
2017-12-08 22:43:14 +0100

    (Day 8) Input handling can be simplified a lot.
    Also fixed the program for the case where the input would only contain
    negative values.

--- Day08/main.cpp
+++ Day08/main.cpp
@@ -1,33 +1,17 @@
 #include <algorithm>
 #include <cassert>
 #include <iostream>
-#include <sstream>
 #include <string>
 #include <unordered_map>
 
 int main(void)
 {
-    int total_max = 0;
     std::unordered_map<std::string, int> regs;
+    std::string reg1, reg2, op, comp;
+    int val1, val2, total_max{INT_MIN};
 
-    for (std::string line; std::getline(std::cin, line);)
+    while (std::cin >> reg1 >> op >> val1 >> reg2 >> reg2 >> comp >> val2)
     {
-        std::istringstream iss{ line };
-        std::string reg;
-        std::string op;
-        std::string comp;
-        std::string reg2;
-        int val;
-        int val2;
-
-        iss >> reg;
-        iss >> op;
-        iss >> val;
-        iss >> reg2; // skip the if
-        iss >> reg2;
-        iss >> comp;
-        iss >> val2;
-
         if (((comp == ">=") && (regs[reg2] >= val2)) ||
             ((comp == ">")  && (regs[reg2] > val2))  ||
             ((comp == "==") && (regs[reg2] == val2)) ||
@@ -35,11 +19,11 @@ int main(void)
             ((comp == "<=") && (regs[reg2] <= val2)) ||
             ((comp == "<")  && (regs[reg2] < val2)))
         {
-            if      (op == "inc") { regs[reg] += val; }
-            else if (op == "dec") { regs[reg] -= val; }
+            if      (op == "inc") { regs[reg1] += val1; }
+            else if (op == "dec") { regs[reg1] -= val1; }
             else                  { assert(false); }
 
-            if (regs[reg] > total_max) { total_max = regs[reg]; }
+            if (regs[reg1] > total_max) { total_max = regs[reg1]; }
         }
     }
 
