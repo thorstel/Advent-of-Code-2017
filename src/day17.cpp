a4a25b2d90ac3486f0da8414f2f7c046fd350a4c
thorstel <thorstil@gmail.com>
2017-12-17 13:58:29 +0100

    Solution for Day 17.

--- src/day17.cpp
+++ src/day17.cpp
@@ -1,9 +1,35 @@
+#include <deque>
+
 #include "default_includes.hpp"
 #include "solution.hpp"
 
 template<>
 void solve<Day17>(std::istream& ins, std::ostream& outs)
 {
-    if (!ins.good()) { outs << "Failed to open input file!" << std::endl; }
+    NOT_USED(ins);
+
+    const auto      steps{343}; // input
+    auto            pos{0};
+    std::deque<int> buffer;
+    buffer.emplace_back(0);
+
+    for (auto i{1}; i <= 2017; ++i)
+    {
+        pos = ((pos + steps) % buffer.size()) + 1;
+        buffer.emplace((buffer.begin() + pos), i);
+    }
+
+    outs << "(Part 1) Value after 2017 = " << buffer[pos + 1] << std::endl;
+
+    auto value{0};
+    pos = 0;
+    for (auto i{1}; i <= 50000000; ++i)
+    {
+        // 0 will remain at index 0, therefore we can just update the
+        // value if insertion index is 1.
+        if ((pos = ((pos + steps) % i) + 1) == 1) { value = i; }
+    }
+
+    outs << "(Part 2) Value after 0    = " << value << std::endl;
 }
 
